b7a905f12b26562e0119bd23f75481605985862b
yhkim <yhkim0131@gmail.com>
2021-08-03 15:57:31 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Question
+from .models import Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Answer)
---
+++ pybo/templatetags/pybo_filter.py
@@ -0,0 +1,19 @@
+# ---------------------------------- [edit] ---------------------------------- #
+import markdown
+from django import template
+from django.utils.safestring import mark_safe
+# ---------------------------------------------------------------------------- #
+
+register = template.Library()
+
+
+@register.filter
+def sub(value, arg):
+    return value - arg
+
+# ---------------------------------- [edit] ---------------------------------- #
+@register.filter()
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
+# ---------------------------------------------------------------------------- #
